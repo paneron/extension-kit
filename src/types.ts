fd2943f2cd8dea3751c5a4975cf61bd4d768e1dd
Anton Strogonoff <anton@strogonoff.name>
2020-09-29 12:36:48 +0900

    feat: update repository view prop types

--- src/types.ts
+++ src/types.ts
@@ -13,11 +13,40 @@ interface ObjectContentSet {
   [objectPath: string]: string | null
 }
 
+
+export type ObjectsChangedEventHook = (
+  eventCallback: (event: { objects: Record<string, true> }) => Promise<void>,
+  args: any[],
+) => void
+
+
+export type UseObjectPathsHook = (
+  query: Record<string, any>
+) => {
+  value: string[]
+  errors: Error[]
+  isUpdating: boolean
+  refresh: () => void
+  _reqCounter: number
+}
+
+
+export type UseObjectDataHook = (
+  objects: Record<string, true>
+) => {
+  value: ObjectContentSet
+  errors: Error[]
+  isUpdating: boolean
+  refresh: () => void
+  _reqCounter: number
+}
+
+
 export interface RepositoryViewProps {
   title: string
-  useRepoContentsChanged:
-    (eventCallback: (event: { workingCopyPath: string, objects: Record<string, true> }) => Promise<void>, args: any[]) => void
-  readObjects: (objects: Record<string, true>) => Promise<ObjectContentSet>
+  useObjectsChangedEvent: ObjectsChangedEventHook
+  useObjectPaths: UseObjectPathsHook
+  useObjectData: UseObjectDataHook
   changeObjects: (changeset: ObjectContentSet, commitMessage: string) => Promise<{
     success: true
   }>
