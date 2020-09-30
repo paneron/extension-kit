c3d84fb24457218c87a36a82de294a39443422d1
Anton Strogonoff <anton@strogonoff.name>
2020-09-30 20:22:22 +0900

    chore: expand types; release alpha1

--- src/types.ts
+++ src/types.ts
@@ -3,17 +3,19 @@ import type React from 'react';
 
 export interface MainPlugin {}
 
+type PluginComponentProps = Pick<RepositoryViewProps, 'React'>;
+export type PluginFC<T> = React.FC<PluginComponentProps & T>;
 export type RendererPlugin = Promise<{
-  repositoryView?: React.FC<RepositoryViewProps>
+  repositoryView?: PluginFC<any>
 }>
 
 export type Plugin = MainPlugin | RendererPlugin;
 
-// TODO: Duplication in Paneron core
-interface ObjectDataset {
+
+export interface ObjectDataset {
   [objectPath: string]: ObjectData
 }
-type ObjectData = null
+export type ObjectData = null
   | { value: string, encoding: string }
   | { value: Uint8Array, encoding: undefined }
 interface ObjectQuery {
@@ -21,41 +23,54 @@ interface ObjectQuery {
   contentSubstring?: string
 }
 
+export type ObjectChange =
+  | { newValue: string | null, encoding: string, oldValue?: string | null }
+  | { newValue: Uint8Array | null, encoding: undefined, oldValue?: Uint8Array | null }
+export interface ObjectChangeset {
+  // Object path must be supplied / is returned relative to repository root.
+  // Writing null must cause the object to be deleted.
+  // When null is returned, it means object does not exist.
+  [objectPath: string]: ObjectChange
+}
+
+export interface CommitOutcome {
+  newCommitHash?: string
+  conflicts?: {
+    [objectPath: string]: true
+  }
+}
+
 
 export type ObjectsChangedEventHook = (
-  eventCallback: (event: { objects: Record<string, true> }) => Promise<void>,
+  // TODO: Duplicated in Paneron core
+  eventCallback: (event: { objects?: Record<string, 'added' | 'modified' | 'removed' | true> }) => Promise<void>,
   args: any[],
 ) => void
 
 
-export type UseObjectPathsHook = (
-  query: ObjectQuery
-) => {
-  value: string[]
+export interface ValueHook<T> {
+  value: T
   errors: Error[]
   isUpdating: boolean
   refresh: () => void
   _reqCounter: number
 }
 
+// TODO: Duplicated within Paneron core
+export type ObjectDataRequest = Record<string, 'utf-8' | undefined>;
 
-export type UseObjectDataHook = (
-  objects: Record<string, true>
-) => {
-  value: ObjectDataset
-  errors: Error[]
-  isUpdating: boolean
-  refresh: () => void
-  _reqCounter: number
-}
+export type ObjectPathsHook = (query: ObjectQuery) => ValueHook<string[]>
+export type ObjectDataHook = (objects: ObjectDataRequest) => ValueHook<ObjectDataset>
+export type RemoteUsernameHook = () => ValueHook<{ username?: string }>
+
 
 export interface RepositoryViewProps {
   title: string
-  react: typeof React
+  React: typeof React
   useObjectsChangedEvent: ObjectsChangedEventHook
-  useObjectPaths: UseObjectPathsHook
-  useObjectData: UseObjectDataHook
-  changeObjects: (changeset: ObjectDataset, commitMessage: string) => Promise<{
-    success: true
-  }>
+  useObjectPaths: ObjectPathsHook
+  useObjectData: ObjectDataHook
+  useRemoteUsername: RemoteUsernameHook
+  makeRandomID: () => Promise<string>
+  changeObjects: (changeset: ObjectChangeset, commitMessage: string) => Promise<CommitOutcome>
 }
