6073eaacbf0eee04586592f55718d9fb82e8bea8
Anton Strogonoff <anton@strogonoff.name>
2020-12-07 06:10:29 +0900

    fix: add dataset migration types

---
+++ src/types/migrations.ts
@@ -0,0 +1,20 @@
+import { ObjectChangeset } from "./data";
+
+interface DatasetMigrationOptions {
+  datasetRootPath: string
+  versionBefore?: string
+  onProgress: (message: string) => void
+}
+
+/* Changeset describing changes that will make the dataset
+   conform to a version, and that version. */
+interface DatasetMigrationSpec {
+  changeset: ObjectChangeset
+  versionAfter: string
+}
+
+/* Given dataset path and current (‘before’) version, checks files at that path
+   and returns a migration spec.
+
+   If current version is not given, assume that a new dataset is initialized. */
+export type DatasetMigrationFunction = (opts: DatasetMigrationOptions) => Promise<DatasetMigrationSpec>;
