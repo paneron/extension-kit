05619496555a0e642061ad90ee40531adaf0eca8
Anton Strogonoff <anton@strogonoff.name>
2021-01-31 06:55:51 +0000

    feat: export UTF-8<->Uint8Array encoder and decoder

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,7 @@
 export function stripLeadingSlash(fp: string): string {
   return fp.replace(/^\//, '');
 }
+
+export const encoder = new TextEncoder();
+
+export const decoder = new TextDecoder('utf-8');
