cb851af4665fac93eec841453d521a9ec4600cfa
Anton Strogonoff <anton@strogonoff.name>
2021-02-07 16:13:58 +0900

    chore: implement OnlyJSON type

--- src/util.ts
+++ src/util.ts
@@ -5,3 +5,11 @@ export function stripLeadingSlash(fp: string): string {
 export const encoder = new TextEncoder();
 
 export const decoder = new TextDecoder('utf-8');
+
+
+export type OnlyJSON<T> =
+  T extends string | number | boolean | null ? T : 
+  T extends Function ? never : 
+  T extends object
+    ? { [K in keyof T]: OnlyJSON<T[K]> }
+    : never;
