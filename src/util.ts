08314de4a646f6c724eb7086c072a0541d3a7637
Anton Strogonoff <anton@strogonoff.name>
2021-02-07 17:43:37 +0900

    style: use ternary nesting style consistently

--- src/util.ts
+++ src/util.ts
@@ -8,8 +8,10 @@ export const decoder = new TextDecoder('utf-8');
 
 
 export type OnlyJSON<T> =
-  T extends string | number | boolean | null ? T : 
-  T extends Function ? never : 
-  T extends object
-    ? { [K in keyof T]: OnlyJSON<T[K]> }
-    : never;
+  T extends string | number | boolean | null
+  ? T
+  : T extends Function
+    ? never
+    : T extends object
+      ? { [K in keyof T]: OnlyJSON<T[K]> }
+      : never;
