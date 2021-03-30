a79210e4e43986ad888833d994cad32a8d1bbcc5
Anton Strogonoff <anton@strogonoff.name>
2021-03-30 06:49:27 +0000

    chore(types/indexes): type index status properly

--- src/types/indexes.ts
+++ src/types/indexes.ts
@@ -3,7 +3,7 @@ import { Progress } from './progress';
 // Describes status of a filtered (or future custom) object index of a dataset
 export interface IndexStatus {
   objectCount: number
-  progress?: Progress
+  progress?: Progress<'initializing' | 'counting' | 'indexing'>
 }
 
 
