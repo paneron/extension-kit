66471fb45b8a544ec2be0d2e89a7975d65bd95f4
Anton Strogonoff <anton@strogonoff.name>
2021-03-30 06:48:58 +0000

    chore(types/progress): capture phase type as generic argument

--- src/types/progress.ts
+++ src/types/progress.ts
@@ -1,6 +1,6 @@
 // Describes progress of some operation
-export interface Progress {
-  phase: string
+export interface Progress<P extends string = string> {
+  phase: P
   loaded: number
   total: number
 }
