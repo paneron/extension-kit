482d02e8fa114c7f0982ef37d633beb7ff22931a
Anton Strogonoff <anton@strogonoff.name>
2021-04-06 16:52:25 +0900

    chore(Grid): major update, remove status

--- src/widgets/Grid.tsx
+++ src/widgets/Grid.tsx
@@ -6,23 +6,18 @@ import { jsx, css } from '@emotion/core';
 import React, { ComponentType, useEffect, useRef } from 'react';
 import { FixedSizeGrid as Grid, GridChildComponentProps, areEqual } from 'react-window';
 import AutoSizer from 'react-virtualized-auto-sizer';
-import { Colors, Icon, IIconProps, ITagProps, NonIdealState, Spinner, Tag } from '@blueprintjs/core';
+import { Colors, Icon, IIconProps } from '@blueprintjs/core';
 
 
 export interface GridData<P extends Record<string, any> = Record<never, never>> {
   items: string[][] // items chunked into rows
   selectedItem: string | null
-  selectItem: (ref: string | null) => void
+  selectItem: (ref: string | null, extraData?: unknown) => void
   openItem?: (ref: string) => void
   cellWidth: number
   cellHeight: number
   padding: number
   extraData: P
-  status?: {
-    progress?: { phase: string, total?: number, loaded?: number }
-    descriptiveName?: { singular: string, plural: string }
-    totalCount: number
-  }
 }
 
 type ItemDataGetter<P extends Record<string, any> = Record<never, never>> = (width: number) => GridData<P> | null
@@ -30,7 +25,7 @@ type ItemDataGetter<P extends Record<string, any> = Record<never, never>> = (wid
 export interface CellProps<P extends Record<string, any> = Record<never, never>> {
   itemRef: string
   isSelected: boolean
-  onSelect?: () => void
+  onSelect?: (extraData?: unknown) => void
   onOpen?: () => void
   height: number
   width: number
@@ -60,10 +55,10 @@ React.FC<{ getGridData: ItemDataGetter<P>, className?: string }> {
     const ref = _data.items[rowIndex]?.[columnIndex];
     if (ref) {
       return (
-        <div style={style}>
+        <div css={css`position: relative;`} style={style}>
           <CellContents
             isSelected={_data.selectedItem === ref}
-            onSelect={() => _data.selectItem(ref)}
+            onSelect={(extraData) => _data.selectItem(ref, extraData)}
             onOpen={_data.openItem ? () => _data.openItem!(ref) : undefined}
             height={_data.cellHeight}
             width={_data.cellWidth}
@@ -109,49 +104,26 @@ React.FC<{ getGridData: ItemDataGetter<P>, className?: string }> {
             const columnCount = (gridData.items[0] ?? []).length;
             const rowCount = gridData.items.length;
             return (
-              <>
-                <Grid
-                    width={width}
-                    css={css`padding-bottom: ${gridData.cellHeight}px`}
-                    height={height}
-                    columnCount={columnCount}
-                    columnWidth={gridData.cellWidth}
-                    rowCount={rowCount}
-                    rowHeight={gridData.cellHeight}
-                    itemKey={({ columnIndex, rowIndex }) => {
-                      const itemRef = gridData.items[rowIndex]?.[columnIndex];
-                      if (!itemRef) {
-                        return columnIndex * rowIndex;
-                      }
-                      return itemRef;
-                    }}
-                    itemData={gridData}>
-                  {Cell}
-                </Grid>
-                {gridData.status
-                  ? <div css={css`
-                        position: absolute; bottom: 0; left: 0; right: 0;
-                        background: rgba(255, 255, 255, 0.85); color: ${Colors.GRAY3};
-                        display: flex;
-                        flex-flow: row nowrap;
-                        align-items: center;
-                        padding: 5px ${gridData.padding}px; font-size: 80%;`}>
-                      Showing {gridData.status.totalCount} {gridData.status.descriptiveName ? gridData.status.descriptiveName.plural : 'items'}
-                      &emsp;
-                      {gridData.status.progress
-                        ? <Spinner
-                            size={10}
-                            value={gridData.status.progress.loaded && gridData.status.progress.total
-                              ? gridData.status.progress.loaded / gridData.status.progress.total
-                              : undefined}
-                          />
-                        : null}
-                    </div>
-                  : null}
-              </>
+              <Grid
+                  width={width}
+                  height={height}
+                  columnCount={columnCount}
+                  columnWidth={gridData.cellWidth}
+                  rowCount={rowCount}
+                  rowHeight={gridData.cellHeight}
+                  itemKey={({ columnIndex, rowIndex }) => {
+                    const itemRef = gridData.items[rowIndex]?.[columnIndex];
+                    if (!itemRef) {
+                      return columnIndex * rowIndex;
+                    }
+                    return itemRef;
+                  }}
+                  itemData={gridData}>
+                {Cell}
+              </Grid>
             );
           } else {
-            return <NonIdealState title="Nothing to show" icon="heart-broken" />;
+            return null;
           }
         }}
       </AutoSizer>
@@ -164,34 +136,57 @@ interface LabelledGridIconProps {
   isSelected: boolean
   onSelect?: () => void
   onOpen?: () => void
-  iconProps?: IIconProps
-  tagProps?: ITagProps
+  contentClassName?: string
+  entityType?: {
+    iconProps?: IIconProps
+    name: string
+  }
   height: number
   width: number
   padding: number 
 }
 
 export const LabelledGridIcon: React.FC<LabelledGridIconProps> =
-function ({ isSelected, onSelect, onOpen, height, padding, tagProps, iconProps, children }) {
+function ({ isSelected, onSelect, onOpen, height, padding, contentClassName, entityType, children }) {
+  const hPad = Math.floor(padding / 2);
+  const qPad = Math.floor(padding / 4);
   return (
     <div
-        css={css`text-align: center; height: ${height - padding}px; padding: ${padding}px; display: flex; flex-flow: column nowrap; align-items: center; justify-content: space-around;`}>
-      <Icon
-          iconSize={Icon.SIZE_LARGE}
-          icon="blank"
-          intent={isSelected ? 'primary' : undefined}
-          onDoubleClick={onOpen}
-          onClick={onSelect}
-          {...iconProps} />
-      <Tag
-          css={css`cursor: default;`}
-          minimal fill
-          intent={isSelected ? 'primary' : undefined}
-          onDoubleClick={onOpen}
-          onClick={onSelect}
-          {...tagProps}>
+        onClick={onSelect}
+        onDoubleClick={onOpen}
+        css={css`
+          position: absolute; top: 0; left: 0; right: 0; bottom: 0;
+          box-shadow: 1px 1px 0 ${Colors.LIGHT_GRAY5} inset, -1px -1px 0 ${Colors.LIGHT_GRAY1} inset;
+          overflow: hidden;
+          cursor: default;
+          z-index: ${isSelected ? 2 : 1};
+          display: flex; flex-flow: column nowrap; align-items: stretch; justify-content: flex-start;
+          background: ${isSelected ? `linear-gradient(345deg, ${Colors.LIGHT_GRAY3}, white)` : Colors.LIGHT_GRAY5};`}>
+      {entityType
+        ? <div css={css`
+              z-index: 3; font-size: 80%; margin: 1px;
+              padding: ${qPad - 1}px ${padding}px ${qPad}px ${padding}px;
+              background: ${isSelected ? Colors.BLUE2 : Colors.LIGHT_GRAY3};
+              letter-spacing: -0.03em;
+              color: ${isSelected ? Colors.LIGHT_GRAY4 : Colors.GRAY2};`}>
+            {entityType.iconProps
+              ? <Icon
+                  iconSize={10}
+                  icon="blank"
+                  css={css`margin-right: ${hPad}px;`}
+                  color={isSelected ? 'white' : Colors.GRAY3}
+                  intent={isSelected ? 'primary' : undefined}
+                  {...entityType.iconProps}
+                />
+              : null}
+            {entityType.name}
+          </div>
+        : null}
+      <div
+          css={css`z-index: 2; flex: 1; font-size: 90%; padding: ${hPad}px ${padding}px;`}
+          className={contentClassName}>
         {children}
-      </Tag>
+      </div>
     </div>
   );
 };
