d5afda06b03b07c7038176b9f9b7c597e85c24bf
Anton Strogonoff <anton@strogonoff.name>
2021-09-11 23:00:42 +0900

    perf(useDebounce): optimize logic, perhaps

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,13 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 
 export function useDebounce<T>(value: T, wait: number): T {
   let [debouncedValue, setDebouncedValue] = useState(value);
+  const setDebounced = useCallback(() => setDebouncedValue(value), [value]);
 
   useEffect(() => {
-    let timeout = setTimeout(() => setDebouncedValue(value), wait);
-    return () => clearTimeout(timeout);
-  }, [value]);
+    if (wait > 0) {
+      let timeout = setTimeout(setDebounced, wait);
+      return () => clearTimeout(timeout);
+    } else {
+      setImmediate(setDebounced);
+      return () => void 0;
+    }
+  }, [value, wait]);
 
   return debouncedValue;
 }
