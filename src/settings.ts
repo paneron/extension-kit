77c8a0919557102e16a462ef2c3c7d43638804d5
Anton Strogonoff <anton@strogonoff.name>
2021-11-04 12:11:51 +0100

    feat(settings): add mainNavbarPosition optino

--- src/settings.ts
+++ src/settings.ts
@@ -2,8 +2,10 @@ export type Settings = Record<string, any>
 
 export interface GlobalSettings extends Settings {
   sidebarPosition: 'left' | 'right'
+  mainNavbarPosition: 'top' | 'bottom'
 }
 
 export const INITIAL_GLOBAL_SETTINGS: GlobalSettings = {
   sidebarPosition: 'right',
+  mainNavbarPosition: 'top',
 };
