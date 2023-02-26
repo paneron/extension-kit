3eca9de28d3ea8e7e223fcc4d18ae18bae118ee3
Anton <anton@strogonoff.name>
2023-02-26 21:46:23 +0800

    docs(types/changes): docstrings for changes/changesets

--- src/types/changes.ts
+++ src/types/changes.ts
@@ -10,27 +10,33 @@ export interface PathChanges {
   [path: string]: ChangeStatus
 }
 
+/**
+ * Describes a change of an object.
+ *
+ * A null value below means nonexistend object at this path.
+ *
+ * - newValue: null means delete object, if it exists.
+ * - oldValue: null means the object previously did not exist.
+ * - Undefined (unspecified) oldValue means no consistency check.
+ */
 export type Change<T extends Uint8Array | Record<string, any>> =
   { newValue: T | null, oldValue?: T | null };
-  // A null value below means nonexistend object at this path.
-  // newValue: null means delete object, if it exists.
-  // oldValue: null means the object previously did not exist.
-  // Undefined oldValue means no consistency check
 
 export type Changeset<C extends Change<any>> = {
   [path: string]: C
 }
 
-/* The result of applying a changeset.
-
-   `conflicts` shows how paths don’t match reference data (`oldValue`s)
-   provided by the changeset. Ideally conflicts would be an empty object,
-
-   Paths in conflicts can be object or buffer paths,
-   depending on use case.
-*/
-// TODO: “Commit hash” ties this to Git; potentially this could be made generic.
+/**
+ * The result of applying a changeset.
+ *
+ * `conflicts` shows how paths don’t match reference data (`oldValue`s)
+ * provided by the changeset. Ideally conflicts would be an empty object,
+ *
+ * Paths in conflicts can be object or buffer paths,
+ * depending on use case.
+ */
 export interface CommitOutcome {
+  // TODO: “Commit hash” ties this to Git; potentially this could be made generic.
   newCommitHash?: string
   conflicts?: PathChanges
 }
