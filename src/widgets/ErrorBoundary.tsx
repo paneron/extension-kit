3902fc7808ce7c03f809dd27e1cafd2c98602acc
Anton <anton@strogonoff.name>
2023-04-19 15:45:49 +0800

    feat(widgets): +ErrorBoundary

---
+++ src/widgets/ErrorBoundary.tsx
@@ -0,0 +1,28 @@
+/** @jsx jsx */
+/** @jsxFrag React.Fragment */
+
+import React from 'react';
+import { jsx } from '@emotion/react';
+import ErrorState from './ErrorState';
+
+
+class ErrorBoundary extends React.Component<{ viewName?: string; }, { error?: string; }> {
+  constructor(props: { viewName: string; }) {
+    super(props);
+    this.state = { error: undefined };
+  }
+  componentDidCatch(error: Error, info: any) {
+    console.error("Error rendering view", this.props.viewName, error, info);
+    this.setState({ error: `${error.name}: ${error.message}` });
+  }
+  render() {
+    if (this.state.error !== undefined) {
+      return <ErrorState viewName={this.props.viewName} technicalDetails={this.state.error} />;
+    }
+    return this.props.children;
+  }
+}
+
+
+export default ErrorBoundary;
+
