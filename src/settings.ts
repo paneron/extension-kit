bb555c8169b41219343853d0378bcf7310e11ef9
Anton <anton@strogonoff.name>
2023-04-29 20:10:14 +0800

    feat(settings): add `defaultTheme` (dark support)

--- src/settings.ts
+++ src/settings.ts
@@ -3,9 +3,13 @@ export type Settings = Record<string, any>
 export interface GlobalSettings extends Settings {
   sidebarPosition: 'left' | 'right'
   mainNavbarPosition: 'top' | 'bottom'
+
+  /** Theme to prefer, if `null` then use system theme. */
+  defaultTheme: 'dark' | 'light' | null
 }
 
 export const INITIAL_GLOBAL_SETTINGS: GlobalSettings = {
   sidebarPosition: 'right',
   mainNavbarPosition: 'top',
+  defaultTheme: 'light',
 };
