5a00d78bed50475238736416c1205affb8abf669
Anton <anton@strogonoff.name>
2023-05-25 16:14:59 +0800

    style: trailing whitespace

--- src/types/buffers.ts
+++ src/types/buffers.ts
@@ -3,10 +3,10 @@
  * A buffer is a raw binary blob
  * and in current implementation maps to a file on disk
  * (or an object in Git repo).
- * 
+ *
  * A buffer is associated with a path (in POSIX format)
  * that describes its place in data repository tree.
- * 
+ *
  * Multiple buffers may comprise a single logical **object**.
  */
 
