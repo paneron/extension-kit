4045fd83d4f20351a1acbc958f8e9d5a0bfaa354
Anton <anton@strogonoff.name>
2023-06-10 19:09:19 +0800

    docs: suggest `use-debounce` over our `useDebounce`

--- src/useDebounce.ts
+++ src/useDebounce.ts
@@ -1,6 +1,9 @@
 import { useCallback, useEffect, useState } from 'react';
 
 
+/**
+ * @deprecated it’s recommended to use the `use-debounce` NPM package instead.
+ */
 export function useDebounce<T>(value: T, wait: number): T {
   let [debouncedValue, setDebouncedValue] = useState(value);
   const setDebounced = useCallback(() => setDebouncedValue(value), [value]);
