bbe19bf34211edc2ad812997d91153adc4ab7f13
Anton <anton@strogonoff.name>
2023-08-28 19:43:43 +0800

    fix: closures breaking OperationQueue

--- src/widgets/OperationQueue/index.tsx
+++ src/widgets/OperationQueue/index.tsx
@@ -2,7 +2,7 @@
 /** @jsxFrag React.Fragment */
 
 import { jsx, css } from '@emotion/react';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import {
   ProgressBar,
   type ToasterInstance,
@@ -13,12 +13,53 @@ import Context from './context';
 
 const OperationQueueContextProvider: React.FC<{ toaster: ToasterInstance }> =
 function ({ toaster, children }) {
-  const [_operationKey, setOperationKey] = useState<string | undefined>(undefined);
   const [_opKeys, setOpKeys] = useState<Map<string, number>>(new Map());
 
   const [_lockingOperationKey, setLockingOperationKey] = useState<string | null>(null);
 
   const isBusy = _lockingOperationKey !== null && _lockingOperationKey !== undefined;
+
+  const ops = [_lockingOperationKey, ...Object.entries(_opKeys).flat()];
+
+  const notify = useCallback(function _notify(opKey: string) {
+    const opCount =  _opKeys.get(opKey) ?? 0;
+
+    if (opCount < 1) {
+      toaster.show({ message: `Done ${opKey}`, intent: 'success', icon: 'tick-circle' }, opKey);
+    } else {
+      toaster.show({
+        message: <div css={css`display: flex; flex-flow: row nowrap; white-space: nowrap; align-items: center;`}>
+          <ProgressBar intent="primary" css={css`width: 50px;`} />
+          &emsp;
+          {opKey}{opCount > 1 ? <>&nbsp;({opCount})</> : null}…
+        </div>,
+        intent: 'primary',
+        timeout: 0,
+      }, opKey);
+    }
+  }, ops);
+
+  const unqueue = useCallback(function _unqueue(opKey: string, isBlocking: boolean) {
+    if (isBlocking) {
+      setLockingOperationKey(null);
+    }
+
+    setOpKeys(_opKeys.set(opKey, (_opKeys.get(opKey) ?? 1) - 1));
+    notify(opKey);
+  }, ops);
+
+  const queue = useCallback(function _queue(opKey: string, isBlocking: boolean) {
+    if (_lockingOperationKey && isBlocking) {
+      toaster.show({ message: `Can’t handle ${opKey}: already busy`, intent: 'warning' });
+      throw new Error("Failed to queue operation: queue busy");
+    }
+    if (isBlocking) {
+      setLockingOperationKey(opKey);
+    }
+
+    setOpKeys(_opKeys.set(opKey, (_opKeys.get(opKey) ?? 0) + 1));
+    notify(opKey);
+  }, ops);
 
   return (
     <Context.Provider value={{
@@ -29,53 +70,17 @@ function ({ toaster, children }) {
         // We block by default for backwards compatibility.
         const isBlocking = opts?.blocking !== false;
 
-        function unqueue() {
-          setOperationKey(undefined);
-          if (isBlocking) {
-            setLockingOperationKey(null);
-          }
-          setOpKeys(map => map.set(opKey, (map.get(opKey) ?? 1) - 1));
-        }
-
         return async (...args) => {
           if (!toaster) {
             throw new Error("Failed to perform operation: toaster not initialized");
           }
-          if (_lockingOperationKey && isBlocking) {
-            console.log("BLOCKED!", gerund, isBlocking, _lockingOperationKey);
-            toaster.show({ message: `Can’t handle ${gerund}: already busy`, intent: 'warning' });
-            throw new Error("Failed to queue operation: queue busy");
-          }
 
-          setOperationKey(opKey);
-          setOpKeys(map => map.set(opKey, (map.get(opKey) ?? 0) + 1));
-
-          if (isBlocking) {
-            setLockingOperationKey(opKey);
-          }
-
-          const opCount = _opKeys.get(opKey) ?? 0;
-
-          toaster.show({
-            message: <div css={css`display: flex; flex-flow: row nowrap; white-space: nowrap; align-items: center;`}>
-              <ProgressBar intent="primary" css={css`width: 50px;`} />
-              &emsp;
-              {gerund}{opCount > 1 ? <>&nbsp;({opCount})</> : null}…
-            </div>,
-            intent: 'primary',
-            timeout: 0,
-          }, gerund);
+          queue(opKey, isBlocking);
 
           try {
             const result = await func(...args);
 
-            toaster.dismiss(opKey);
-
-            unqueue();
-
-            if (opCount < 1) {
-              toaster.show({ message: `Done ${gerund}`, intent: 'success', icon: 'tick-circle' }, gerund);
-            }
+            unqueue(opKey, isBlocking);
 
             return result;
           } catch (e) {
@@ -88,16 +93,15 @@ function ({ toaster, children }) {
               errMsg = rawErrMsg;
             }
 
-            toaster.dismiss(opKey);
             toaster.show({
               message: `Problem ${gerund}. The error said: “${errMsg}”`,
               intent: 'danger',
               icon: 'error',
               timeout: 0,
               onDismiss: () => {
-                unqueue();
+                unqueue(opKey, isBlocking);
               },
-            }, gerund);
+            }, opKey);
 
             throw e;
           }
