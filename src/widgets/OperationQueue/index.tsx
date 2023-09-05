70cd88338ece09fb774f3cbc2358b8b290892f34
Anton <anton@strogonoff.name>
2023-09-05 18:58:09 +0800

    feat(OperationQueue): make global toaster & queue

--- src/widgets/OperationQueue/index.tsx
+++ src/widgets/OperationQueue/index.tsx
@@ -3,47 +3,57 @@
 
 import { jsx, css } from '@emotion/react';
 import React, { useCallback, useState } from 'react';
-import { ProgressBar, type ToasterInstance } from '@blueprintjs/core';
+import { Toaster, Spinner, SpinnerSize, ProgressBar } from '@blueprintjs/core';
 
 import Context from './context';
 
 
-const OperationQueueContextProvider: React.FC<{ toaster: ToasterInstance }> =
-function ({ toaster, children }) {
-  const [_opKeys, setOpKeys] = useState<Map<string, number>>(new Map());
+const toaster = Toaster.create({ position: 'bottom' });
 
+const _opKeys: Map<string, number> = new Map();
+
+function notify(opKey: string, blocking: boolean) {
+  const opCount =  _opKeys.get(opKey) ?? 0;
+
+  if (opCount < 1) {
+    toaster.show({
+      message: `Done ${opKey}`,
+      intent: blocking ? 'success' : undefined,
+      icon: 'tick-circle',
+    }, opKey);
+  } else {
+    toaster.show({
+      message: <div css={css`display: flex; flex-flow: row nowrap; white-space: nowrap; align-items: center;`}>
+        {blocking
+          ? <ProgressBar intent="primary" css={css`width: 50px;`} />
+          : <Spinner size={SpinnerSize.SMALL} />}
+        &emsp;
+        {opKey}{opCount > 1 ? <>&nbsp;({opCount})</> : null}…
+      </div>,
+      intent: blocking ? 'primary' : undefined,
+      timeout: 0,
+    }, opKey);
+  }
+};
+
+
+/**
+ * Handles singleton, app-global operation queue context.
+ */
+const OperationQueueContextProvider: React.FC<Record<never, never>> =
+function ({ children }) {
   const [_lockingOperationKey, setLockingOperationKey] = useState<string | null>(null);
 
   const isBusy = _lockingOperationKey !== null && _lockingOperationKey !== undefined;
-
-  const ops = [_lockingOperationKey, ...Object.entries(_opKeys).flat()];
-
-  const notify = useCallback(function _notify(opKey: string) {
-    const opCount =  _opKeys.get(opKey) ?? 0;
-
-    if (opCount < 1) {
-      toaster.show({ message: `Done ${opKey}`, intent: 'success', icon: 'tick-circle' }, opKey);
-    } else {
-      toaster.show({
-        message: <div css={css`display: flex; flex-flow: row nowrap; white-space: nowrap; align-items: center;`}>
-          <ProgressBar intent="primary" css={css`width: 50px;`} />
-          &emsp;
-          {opKey}{opCount > 1 ? <>&nbsp;({opCount})</> : null}…
-        </div>,
-        intent: 'primary',
-        timeout: 0,
-      }, opKey);
-    }
-  }, ops);
 
   const dequeue = useCallback(function _dequeue(opKey: string, isBlocking: boolean) {
     if (isBlocking) {
       setLockingOperationKey(null);
     }
 
-    setOpKeys(_opKeys.set(opKey, (_opKeys.get(opKey) ?? 1) - 1));
-    notify(opKey);
-  }, ops);
+    _opKeys.set(opKey, (_opKeys.get(opKey) ?? 1) - 1);
+    notify(opKey, isBlocking);
+  }, [_lockingOperationKey]);
 
   const queue = useCallback(function _queue(opKey: string, isBlocking: boolean) {
     if (_lockingOperationKey && isBlocking) {
@@ -54,14 +64,14 @@ function ({ toaster, children }) {
       setLockingOperationKey(opKey);
     }
 
-    setOpKeys(_opKeys.set(opKey, (_opKeys.get(opKey) ?? 0) + 1));
-    notify(opKey);
-  }, ops);
+    _opKeys.set(opKey, (_opKeys.get(opKey) ?? 0) + 1);
+    notify(opKey, isBlocking);
+  }, [_lockingOperationKey]);
 
   return (
     <Context.Provider value={{
       isBusy,
-      performOperation: function performOperation(gerund, func, opts) {
+      performOperation: useCallback(function performOperation(gerund, func, opts) {
         const opKey = gerund;
 
         // We block by default for backwards compatibility.
@@ -92,7 +102,7 @@ function ({ toaster, children }) {
 
             toaster.show({
               message: `Problem ${gerund}. The error said: “${errMsg}”`,
-              intent: 'danger',
+              intent: isBlocking ? 'danger' : 'warning',
               icon: 'error',
               timeout: 0,
               onDismiss: () => {
@@ -103,7 +113,7 @@ function ({ toaster, children }) {
             throw e;
           }
         }
-      },
+      }, [queue, dequeue]),
     }}>
       {children}
     </Context.Provider>
