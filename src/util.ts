aecbcc132ec12cdae08d8e920341e82d7d925b7a
Anton <anton@strogonoff.name>
2023-09-12 16:28:52 +0800

    feat(util): add object normalization functions

--- src/util.ts
+++ src/util.ts
@@ -22,6 +22,7 @@ export type OnlyJSON<T> =
       ? { [K in keyof T]: OnlyJSON<T[K]> }
       : never;
 
+
 /**
  * Normalizes object by ensuring its keys are sorted.
  * May not be the most efficient way of doing so.
@@ -29,3 +30,75 @@ export type OnlyJSON<T> =
 export function normalizeObject<T extends Record<string, any>>(obj: T): T {
   return Object.fromEntries(Object.entries(obj).sort()) as T;
 }
+
+export function toJSONPreservingUndefined(data: any): string {
+  return (JSON.
+    stringify(
+      data || {},
+      (_, v) => (v === undefined) ? '__undefined' : v).
+    replace(/\"__undefined\"/g, 'undefined'));
+}
+
+/**
+ * NOTE: This is NOT a very fast implementation yet. Try to avoid.
+ */
+export function JSONStringifyNormalized(val: any) {
+  return toJSONPreservingUndefined(normalizeVal(val));
+}
+
+export function objectsHaveSameShape(l: any, r: any): boolean {
+  return JSONStringifyNormalized(l) === JSONStringifyNormalized(r);
+}
+
+/**
+ * Does the equivalent of `normalizeObject` on given object
+ * as well as any objects encountered within.
+ */
+function normalizeObjectRecursively
+<T extends Record<string, any>>
+(obj: T, _seen: null | WeakSet<any> = null): T {
+  const seen: WeakSet<any> = _seen ?? new WeakSet();
+
+  if (seen.has(obj)) {
+    return obj;
+  }
+
+  if (isObject(obj)) {
+    seen.add(obj);
+
+    const normalized = Object.fromEntries(
+      Object.entries(obj).
+        sort().
+        map(([key, val]) => [
+          key,
+          normalizeVal(val, seen),
+        ])
+      ) as T;
+
+    seen.delete(obj);
+
+    return normalized;
+  } else {
+    throw new Error(`Not an object: ${obj}`);
+  }
+}
+
+function normalizeVal<T>(val: T, _seen: null | WeakSet<any> = null): T {
+  const seen: WeakSet<any> = _seen ?? new WeakSet();
+
+  if (Array.isArray(val)) {
+    return val.map(v => isObject(v) ? normalizeObjectRecursively(v, seen) : v) as unknown as T;
+  } else if (isObject(val)) {
+    return normalizeObjectRecursively(val, seen);
+  } else {
+    return val;
+  }
+}
+
+function isObject(val: unknown): val is Record<string, any> {
+  return val !== null && typeof val === 'object' && !Array.isArray(val);
+}
+
+//function isArray(val: unknown): val is unknown[] {
+//  return val !== null && typeof val === 'object' && Array.isArray(val);
+//}
