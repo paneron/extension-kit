f52a0b6f9eb0e99896cac1b8e893fe32ff50a2f4
Anton <anton@strogonoff.name>
2023-11-08 21:23:32 +0800

    cleanup(widgets): remove DLFlex (fragile layout)

--- src/widgets/DL.tsx
+++ src/widgets/DL.tsx
@@ -22,22 +22,4 @@ const DL = styled.dl`
   }
 `;
 
-/**
- * Definition list where definitions can be very long
- * and therefore scrollable.
- *
- * Same rule as DL (DT+DD combos must have wrappers).
- *
- * DD with large content can have overflow-y: auto.
- */
-export const DLFlex = styled(DL)`
-  overflow: hidden;
-  display: flex;
-  flex-flow: column nowrap;
-  > * {
-    /** NOTE: CONSTRAINT: Same as surrounding line height. */
-    min-height: 1.28em;
-  }
-`;
-
 export default DL;
