7011318fd1acbcc3c25f9fa9e50c4fdc3e882ccd
Anton <anton@strogonoff.name>
2023-11-18 23:04:01 +0800

    docs: convert a comment to proper docstring

--- src/object-specs/yaml/schema.ts
+++ src/object-specs/yaml/schema.ts
@@ -3,7 +3,7 @@ import customTimestampType from './custom-timestamp';
 import { customBoolType } from './custom-bool';
 
 
-/* This schema adds timestamp parsing and adjusts boolean parsing. */
+/** This schema adds timestamp parsing and adjusts boolean parsing. */
 const schema = yaml.DEFAULT_SCHEMA.extend({
   // Trick because js-yaml API appears to not support augmenting implicit tags
   implicit: [
