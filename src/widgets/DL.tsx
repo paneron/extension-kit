bd1555c0f117fa9e1914fc16bf7a61f5f7c36097
Anton <anton@strogonoff.name>
2024-05-02 22:58:37 +0800

    feat(widgets): add DLEntry helper/wrapper to DL

--- src/widgets/DL.tsx
+++ src/widgets/DL.tsx
@@ -1,9 +1,11 @@
+import React, { useMemo, useCallback } from 'react';
 import styled from '@emotion/styled';
 
 
 /**
  * Definition list.
- * Nest wrappers (like divs) with dt and dd within.
+ * Nest wrappers (like divs) with dt and dd within
+ * (`DLEntry` can be used to save time).
  * NOTE: Wrappers are required. Only one DD per DT is allowed.
  *
  * Doesn’t work in narrow containers if DT and/or DD is too long,
@@ -24,6 +26,55 @@ const DL = styled.dl`
     }
   }
 `;
+
+
+/**
+ * For use with `DL`.
+ * Specify
+ */
+export const DLEntry: React.VoidFunctionComponent<{
+  term: JSX.Element | JSX.Element[] | string | string[]
+  definition: JSX.Element | JSX.Element[] | string | string[]
+  style?: React.CSSProperties
+  className?: string
+  dtStyle?: React.CSSProperties
+  dtClassName?: string
+  ddStyle?: React.CSSProperties
+  ddClassName?: string
+}> = function ({
+  term, definition,
+  style, className,
+  dtStyle, dtClassName,
+  ddStyle, ddClassName,
+}) {
+
+  const DT = useCallback(
+    ((t: string | JSX.Element) =>
+      <dt className={dtClassName} style={dtStyle}>{t}</dt>),
+    [dtClassName, dtStyle]);
+
+  const DD = useCallback(
+    ((d: string | JSX.Element) =>
+      <dd className={ddClassName} style={ddStyle}>{d}</dd>),
+    [dtClassName, dtStyle]);
+
+  const terms: (JSX.Element | string)[] = useMemo(
+    (() => Array.isArray(term)
+      ? term
+      : [term]),
+    [term]);
+
+  const definitions: (JSX.Element | string)[] = useMemo(
+    (() => Array.isArray(definition)
+      ? definition
+      : [definition]),
+    [definition]);
+
+  return <div className={className} style={style}>
+    {terms.map(DT)}
+    {definitions.map(DD)}
+  </div>
+};
 
 
 /**
